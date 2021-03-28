22868c6a91fbe7b0a7b0529b808caf8e82fcff6c
Mustafa Hesham <50930361+mustafa-hesham@users.noreply.github.com>
2021-03-29 01:47:52 +0200

    Update settings.py
    removed secret key

--- findmedicalservices/settings.py
+++ findmedicalservices/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v#huu%^$1$jz8-ignq9ozwk7z74-^js!jl)(_9rl^@+*f#cny9'
+SECRET_KEY = 'Your secret key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
